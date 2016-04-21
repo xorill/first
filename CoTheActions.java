4031723436ba6f16ea80f35b4ec6cdb2def75ff1
Laczo Gyula <gy.laczo@gmail.com>
2016-04-21 23:20:42 +0200

    Some menu actions

---
+++ CoTheActions.java
@@ -0,0 +1,39 @@
+package proba;
+
+public class CoTheActions {
+
+	
+	CoTheActions() {   														//konstruktor
+	}
+	
+	void startMultiGame() {
+		
+	System.out.println("multi game starting");
+	
+	}
+
+	void startSingleGame() {
+	
+	System.out.println("single game starting");
+	
+	}
+	
+	void saveGame(){
+	
+	System.out.println("saving game");
+		
+	}
+	
+	void startDisconnect(){
+		
+	System.out.println("disconnect starting");
+			
+	}
+	
+	void startLoad(){
+		
+	System.out.println("Loading game");
+				
+	}
+	
+}
