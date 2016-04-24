f6c54eec237b80e9a29a9005fa3bb363c99a6872
Laczo Gyula <gy.laczo@gmail.com>
2016-04-24 18:32:17 +0200

    Main function added

---
+++ Main.java
@@ -0,0 +1,13 @@
+package proba;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Program starts");
+		GuiOfTheGame gui = new GuiOfTheGame(new CoTheActions());
+		
+
+	}
+
+}
